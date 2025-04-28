96cbd2a4266fadd3b34305c9730d23a8a14628b6
Success <ajibolasuccess17@gmail.com>
2025-04-28 17:05:59 +0100

    reinstated

--- src/components/Pages/Search.jsx
+++ src/components/Pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import NewProducts from '../../assets/NewProducts'
+import NewProducts from '../../assets/Newproducts'
 import Card from '../commonfiles/Card'
 
 const Search = () => {
