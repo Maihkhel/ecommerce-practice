b0b05ade1955584442e0fd1498bf8041e7353673
Success <ajibolasuccess17@gmail.com>
2025-04-29 16:09:56 +0100

    added products

--- src/assets/Newproducts.jsx
+++ src/assets/Newproducts.jsx
@@ -1,3 +1,8 @@
+import Fashion from './images/fashion2.jpg'
+import flower from './images/FLOWER.jpg'
+import flower1 from './images/FLOWER1.jpg'
+import Gaming from './images/GAME.jpg'
+import Code from './images/code.jpg'
 import MOUNTAIN from './images/mountain.jpg'
 import FLOWER2 from './images/flower3.png'
 import FLOWER3 from './images/floweragain.png'
@@ -12,9 +17,52 @@ import PHONE3 from './images/phone3.webp'
 import PHONE4 from './images/phone4.webp'
 
 
-const NewProducts =[
-{
-        id:6,
+const NewProducts = [
+
+    {
+        id: 1,
+        name: 'fashion style',
+        img: Fashion,
+        new_price: 150.00,
+        old_price: 175.00,
+        category: 'flower'
+    },
+    {
+        id: 2,
+        name: 'flower',
+        img: flower,
+        new_price: 100.00,
+        old_price: 125.00,
+        category: 'flower'
+    },
+    {
+        id: 3,
+        name: 'Another flower',
+        img: flower1,
+        new_price: 70.00,
+        old_price: 95.00,
+        category: 'flower'
+    },
+    {
+        id: 4,
+        name: 'Game',
+        img: Gaming,
+        new_price: 175.00,
+        old_price: 125.00,
+        category: 'game'
+    },
+    {
+        id: 5,
+        name: 'Code everyday',
+        img: Code,
+        new_price: 220.00,
+        old_price: 200.00,
+        category: 'code'
+    },
+
+
+    {
+        id: 6,
         name: 'A Huge Rock',
         category: 'Rock',
         img: MOUNTAIN,
@@ -22,7 +70,7 @@ const NewProducts =[
         new_price: 40.90
     },
     {
-        id:7,
+        id: 7,
         name: 'Flower2',
         category: 'flower',
         img: FLOWER2,
@@ -30,7 +78,7 @@ const NewProducts =[
         new_price: 100.90
     },
     {
-        id:8,
+        id: 8,
         name: 'Flower3',
         category: 'flower',
         img: FLOWER3,
@@ -38,7 +86,7 @@ const NewProducts =[
         new_price: 79.90
     },
     {
-        id:9,
+        id: 9,
         name: 'city',
         category: 'Building',
         img: CITY,
@@ -46,7 +94,7 @@ const NewProducts =[
         new_price: 109.90
     },
     {
-        id:10,
+        id: 10,
         name: 'city',
         category: 'Building',
         img: TOWER,
@@ -54,7 +102,7 @@ const NewProducts =[
         new_price: 189.90
     },
     {
-        id:11,
+        id: 11,
         name: 'vase',
         category: 'flowervase',
         img: VASE1,
@@ -62,7 +110,7 @@ const NewProducts =[
         new_price: 99.90
     },
     {
-        id:12,
+        id: 12,
         name: 'vase2',
         category: 'flowervase',
         img: VASE2,
@@ -70,7 +118,7 @@ const NewProducts =[
         new_price: 209.90
     },
     {
-        id:13,
+        id: 13,
         name: 'vase3',
         category: 'flowervase',
         img: VASE3,
@@ -78,46 +126,46 @@ const NewProducts =[
         new_price: 150.90
     },
 
-     {
-            id: 13,
-            name: 'Samsung Oxygen',
-            category: 'phone',
-            img: PHONE1,
-            old_price: 5000,
-            new_price: 6000.70,
-            description:
-                'Samsung Oxygen is a high-performance smartphone built for users who demand speed, clarity, and functionality. It features an expansive AMOLED display, robust processor, and high-resolution camera system. Designed for multitasking and entertainment, it supports fast charging and has a long-lasting battery. Whether for gaming, photography, or work, the Oxygen offers a smooth, lag-free experience. Its sleek, modern design is complemented by Samsung’s signature software optimizations. Ideal for tech enthusiasts looking for power and reliability.'
-        },
-        {
-            id: 14,
-            name: 'Gallaxy s20 pro',
-            category: 'phone',
-            img: PHONE2,
-            old_price: 7000,
-            new_price: 7000.90,
-            description:
-                'The Galaxy S20 Pro is Samsung’s flagship device that redefines mobile performance. It boasts a professional-grade camera system, including 8K video, ultra-wide lens, and up to 100x zoom. Its 120Hz display delivers silky-smooth visuals, perfect for gaming and video. With 5G connectivity, a high-capacity battery, and top-tier processors, this phone is future-ready. Whether streaming, shooting, or scrolling, the S20 Pro provides a seamless experience in a sleek, premium package.'
-        },
-        {
-            id: 15,
-            name: 'Iphone 14',
-            category: 'phone',
-            img: PHONE3,
-            old_price: 9000,
-            new_price: 12000.90,
-            description:
-                'The iPhone 14 is Apple’s iconic smartphone reimagined with powerful enhancements. It features a super retina XDR display, enhanced battery life, and an advanced dual-camera system with night mode and cinematic video. Built with the latest A-series chip, it ensures ultra-fast performance and energy efficiency. Seamless iOS integration, robust security features, and a sleek, durable design make the iPhone 14 a leading choice for both tech lovers and everyday users. It’s innovation, refined.'
-        },
-        {
-            id: 16,
-            name: 'Iphone x',
-            category: 'phone',
-            img: PHONE4,
-            old_price: 5000,
-            new_price: 5000.20,
-            description:
-                'iPhone X represents a turning point in smartphone design, offering a borderless OLED screen, Face ID technology, and wireless charging. It was Apple’s first device to eliminate the home button, ushering in a new era of gesture-based interaction. Though no longer the latest, the iPhone X remains a reliable and stylish option for users seeking a premium Apple experience without breaking the bank. It balances performance, design, and software support impressively.'
-        },
+    {
+        id: 13,
+        name: 'Samsung Oxygen',
+        category: 'phone',
+        img: PHONE1,
+        old_price: 5000,
+        new_price: 6000.70,
+        description:
+            'Samsung Oxygen is a high-performance smartphone built for users who demand speed, clarity, and functionality. It features an expansive AMOLED display, robust processor, and high-resolution camera system. Designed for multitasking and entertainment, it supports fast charging and has a long-lasting battery. Whether for gaming, photography, or work, the Oxygen offers a smooth, lag-free experience. Its sleek, modern design is complemented by Samsung’s signature software optimizations. Ideal for tech enthusiasts looking for power and reliability.'
+    },
+    {
+        id: 14,
+        name: 'Gallaxy s20 pro',
+        category: 'phone',
+        img: PHONE2,
+        old_price: 7000,
+        new_price: 7000.90,
+        description:
+            'The Galaxy S20 Pro is Samsung’s flagship device that redefines mobile performance. It boasts a professional-grade camera system, including 8K video, ultra-wide lens, and up to 100x zoom. Its 120Hz display delivers silky-smooth visuals, perfect for gaming and video. With 5G connectivity, a high-capacity battery, and top-tier processors, this phone is future-ready. Whether streaming, shooting, or scrolling, the S20 Pro provides a seamless experience in a sleek, premium package.'
+    },
+    {
+        id: 15,
+        name: 'Iphone 14',
+        category: 'phone',
+        img: PHONE3,
+        old_price: 9000,
+        new_price: 12000.90,
+        description:
+            'The iPhone 14 is Apple’s iconic smartphone reimagined with powerful enhancements. It features a super retina XDR display, enhanced battery life, and an advanced dual-camera system with night mode and cinematic video. Built with the latest A-series chip, it ensures ultra-fast performance and energy efficiency. Seamless iOS integration, robust security features, and a sleek, durable design make the iPhone 14 a leading choice for both tech lovers and everyday users. It’s innovation, refined.'
+    },
+    {
+        id: 16,
+        name: 'Iphone x',
+        category: 'phone',
+        img: PHONE4,
+        old_price: 5000,
+        new_price: 5000.20,
+        description:
+            'iPhone X represents a turning point in smartphone design, offering a borderless OLED screen, Face ID technology, and wireless charging. It was Apple’s first device to eliminate the home button, ushering in a new era of gesture-based interaction. Though no longer the latest, the iPhone X remains a reliable and stylish option for users seeking a premium Apple experience without breaking the bank. It balances performance, design, and software support impressively.'
+    },
 
 ]
 
